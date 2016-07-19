a9c51ca1d641741c0a14bff22782f11946f9dcca
ame <ameeta.viegas@gmail.com>
2016-07-19 14:20:09 -0400

    step9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
+      # same session, user can log in directly. aka: auto log in
       redirect_to movies_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
   resources :movies
-  resources :users, only:[:new, :create]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   root to: 'movies#index'
 end
