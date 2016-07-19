7bd683180aac3d41e60006634113432cdf572b3d
ame <ameeta.viegas@gmail.com>
2016-07-19 15:23:03 -0400

    step 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'movies#index'
 end
